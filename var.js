90482ca0895e16a7b567ba3003e275df68c5064a
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-15 15:27:42 +0500

    variables in JS

---
+++ var.js
@@ -0,0 +1,56 @@
+let x = 112;
+var y = 212;
+const z = 312;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// let
+function check(){
+    let x = "Jawad";  
+    console.log(x);
+}
+check()
+
+//var
+function check(){
+    var y = "sajjad";  
+    console.log(y);
+}
+check()
+
+// const
+function check(){
+    const z = "hussain";  
+    console.log(z);
+}
+check()
+
+
+// var let  = "fyuger"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Why its output is false?
+// let a = 10, b = 10, c = 10;
+// if(a == b == c){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
